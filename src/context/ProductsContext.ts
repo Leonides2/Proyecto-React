67e538ff561d5120c218c3f79eb8495bbc668772
Leonides2 <leonidesart@hotmail.com>
2024-05-01 14:32:44 -0600

    fixes

--- src/context/ProductsContext.ts
+++ src/context/ProductsContext.ts
@@ -1,14 +1,24 @@
 import React from 'react'
 
+type ProductContext= {
+    productId: number,
+    setProductId: (productId: number) => void,
+    skip: number,
+    setSkip: (skip: number) => void,
+    limit: number,
+    setLimit: (limit: number) => void,
+};
+
+
 
 //COMPLETAR EL CONTEXT CON LO QUE NECESITE
-const ProductsContext = React.createContext({
+const ProductsContext = React.createContext<ProductContext>({
     productId: 0,
-    setProductId: (productId: number) => {},
+    setProductId: () => {},
     skip: 0,
-    setSkip: (skip: number) => {},
+    setSkip: () => {},
     limit: 0,
-    setLimit: (limit: number) => {},
+    setLimit: () => {},
 });
 
 
