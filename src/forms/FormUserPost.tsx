3c2252677f685d264c398f6f60cba377b7a621b3
KarlaAnguloC <angulokarla1201@gmail.com>
2024-05-05 19:50:22 -0600

    Form-POST

--- src/forms/FormUserPost.tsx
+++ src/forms/FormUserPost.tsx
@@ -1,37 +1,85 @@
-
+import { useNavigate } from 'react-router';
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 export const FormUserPost = () => {
     
-    const {register,handleSubmit} = useForm<User>();
-    const onSubmit: SubmitHandler<User> = (data) => console.log(data)
+    const navigate = useNavigate();
+    const {register,handleSubmit, formState:{errors}, setValue} = useForm<User>();
+
+    const onSubmit: SubmitHandler<User> = async(data) => {
+
+        try {
+            const response = await fetch('https://663148cdc92f351c03dcb0e3.mockapi.io/resources/Users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                console.log('Usuario agregado correctamente');
+                navigate("/Proyecto-React");
+            } else {
+                console.error('Error al agregar usuario:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error en la solicitud POST:', error);
+        }
+
+    };
+
+    const formatPhone = (value: string) => {
+        const phoneNumber = value.replace(/\D/g, '');
+        const formattedPhoneNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+        return formattedPhoneNumber;
+    };
 
     return (
         <>
-        <div>
+        <div className="form-container">
         <h2>Agregar Usuario</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
+            <div className="form-group">
                 <label>Nombre: </label>
-                <input type='text' {...register("name")}/>
+                <input type='text' {...register("name",{
+                    required: true,
+                    pattern: /^[A-Za-z\s]+$/
+                })}/>
+                {errors.name?.type === "required" && <p className="error-message">Espacio requerido</p>}
+                {errors.name?.type === "pattern" && <p className="error-message">Solo se permiten letras y espacios</p>}
             </div>
-            <div>
+            <div className="form-group">
                 <label>Trabajo: </label>
-                <input type='text' {...register("job")}/>
+                <input type='text' {...register("job",{
+                    required:true
+                })}/>
+                {errors.job?.type === "required" && <p className="error-message">Espacio requerido</p>}
             </div>
-            <div>
+            <div className="form-group">
                 <label>Telefono: </label>
-                <input type='text' {...register("phone")}/>
+                <input type='text' {...register("phone",{
+                    required:true
+                })}
+                onChange={(e) => setValue('phone', formatPhone(e.target.value))}
+                />
+                {errors.phone?.type === "required" && <p className="error-message">Espacio requerido</p>}
             </div>
-            <div>
+            <div className="form-group">
                 <label>Direccion: </label>
-                <input type='text' {...register("address")}/>
+                <input type='text' {...register("address",{
+                    required:true
+                })}/>
+                {errors.address?.type === "required" && <p className="error-message">Espacio requerido</p>}
             </div>
-            <div>
+            <div className="form-group">
                 <label>Fecha de nacimiento: </label>
-                <input type='text' {...register("birthdate")}/>
+                <input type='text' {...register("birthdate",{
+                    required:true
+                })}/>
+                {errors.birthdate?.type === "required" && <p className="error-message">Espacio requerido</p>}
             </div>
-            <div>
+            <div className="form-group">
                 <input type='submit' value='Enviar'/>
             </div>
 
