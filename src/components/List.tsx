418117d1be9487e876129094ff283fb8347196bd
juanjosecorella <juanjosecorella2004@gmail.com>
2024-05-07 16:01:54 -0600

    Edit Button Test
    Muestra el botón de edición para cada usuario y simplemente muestra su id

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,32 +4,36 @@ import './css/List.css'
 
 const List = ({table}:{table:Table |undefined}) => {
 
-   
+   const editUser = (userId: number) => {
+    alert(`Edit user ${userId}`)
+   }
 
-return (
-    <div className='user-list' id='user-list'>
-    <div className="table-wrapper">
-        <table className="fl-table">
-            <thead>
-                <tr>
-                    {table?.columns.map((column, index) => (
-                        <th key={index}>{column.title}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {table?.columns[0].rows.map((_row, rowIndex) => (
-                    <tr key={rowIndex}>
-                        {table.columns.map((column, colIndex) => (
-                            <td key={colIndex}>{column.rows[rowIndex].content}</td>
+    return (
+        <div className='user-list' id='user-list'>
+        <div className="table-wrapper">
+            <table className="fl-table">
+                <thead>
+                    <tr>
+                        {table?.columns.map((column, index) => (
+                            <th key={index}>{column.title}</th>
                         ))}
+                        <th>Edit User</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-    </div>
-)
+                </thead>
+                <tbody>
+                    {table?.columns[0].rows.map((_row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {table.columns.map((column, colIndex) => (
+                                <td key={colIndex}>{column.rows[rowIndex].content}</td>
+                            ))}
+                            <td><button className="edit-user-button" onClick={() => editUser(Number(table.columns[0].rows[rowIndex].content))}>Edit User</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+        </div>
+    )
 
 }
 
